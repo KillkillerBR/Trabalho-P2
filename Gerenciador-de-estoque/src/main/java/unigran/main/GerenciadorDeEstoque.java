1f5d3c52697f77fa3f70a214299a418bcdf17173
KillkillerBR <lucas.vesco@gmail.com>
2024-05-02 19:30:23 -0400

    Criação do projeto Java

---
+++ Gerenciador-de-estoque/src/main/java/unigran/main/GerenciadorDeEstoque.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package unigran.main;
+
+/**
+ *
+ * @author lucas
+ */
+public class GerenciadorDeEstoque {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
